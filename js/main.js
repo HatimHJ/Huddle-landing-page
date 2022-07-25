c4331f0aa26869a8890360c8fc8732aac34347e6
HatimHJ <hatimaljelwi@gmail.com>
2022-07-25 18:19:03 +0300

    finale #1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
-// const counters = document.querySelectorAll("#counter");
-// const suffix = ["k+", "m+"];
-// const speed = 100;
+AOS.init();
+const body = document.querySelector("body");
+const loader = document.querySelector(".loader");
+const email = document.querySelector("#email");
+const emailbtn = document.querySelector("#submit");
+const form = document.querySelector("form.grid");
 
-// window.addEventListener("scroll", () => {
-// 	counters.forEach((counter) => {
-// 		if (Math.round(counter.getBoundingClientRect().top) < 743) {
-// 			const counterFunction = () => {
-// 				const value = +counter.getAttribute("data-value");
-// 				const initValue = +counter.innerHTML;
-// 				const inc = value / speed;
-// 				if (initValue < value) {
-// 					counter.innerHTML = Math.ceil(initValue + inc);
-// 					setTimeout(() => {
-// 						counterFunction();
-// 					}, 10);
-// 				} else {
-// 					counter.innerHTML = value;
-// 				}
-// 			};
-// 			counterFunction();
-// 		}
-// 	});
-// });
+body.onload = () => {
+	// data-aos-easing="ease-in-sine"
+	loader.setAttribute("data-aos-easing", "linear");
+	loader.setAttribute("data-aos", "slide-left");
+};
+
+function ValidateEmail(mail) {
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+		return true;
+	}
+	return false;
+}
+const span = document.createElement("span");
+span.textContent = "Check Your Email Please";
+
+emailbtn.onclick = (e) => {
+	e.preventDefault();
+	if (email.value === "") {
+		email.style.border = `2px solid #f96262`;
+		form.append(span);
+	}
+	// email Validation
+	else if (!ValidateEmail(email.value)) {
+		email.style.border = `2px solid #f96262`;
+		form.append(span);
+	} else {
+		email.style.border = `1px solid green`;
+		span.remove();
+	}
+};
